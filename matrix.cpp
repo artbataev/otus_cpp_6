84f953c47007ca4301d54c5989c9c3929544a903
artbataev <artbataev@gmail.com>
2018-08-26 23:08:10 +0300

    Partially implemented main logic (can use by index, but not iterate).

--- matrix.cpp
+++ matrix.cpp
@@ -1,3 +1,89 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <map>
+
+template<typename T,
+        int DefaultValue,
+        int NumDimensions = 2>
+class Matrix;
+
+
+template<typename T, int DefaultValue>
+class Matrix<T, DefaultValue, 1> {
+public:
+    Matrix() = default;
+
+    int& operator[](const int idx) {
+        if (elements.count(idx) == 0) elements[idx] = DefaultValue;
+        return elements.at(idx);
+    }
+
+    size_t size() const {
+        size_t result = 0;
+        for(const auto& [key, value]:elements) {
+            if (value != DefaultValue) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+private:
+    std::map<int, int> elements;
+};
+
+template<typename T,
+        int DefaultValue,
+        int NumDimensions>
+class Matrix {
+public:
+    Matrix() = default;
+    constexpr static int NumDimensionsRed = NumDimensions - 1;
+
+    size_t size() const {
+        size_t result = 0;
+        for(const auto& [key, value]:elements) {
+            result += value.size();
+        }
+        return result;
+    }
+
+    Matrix<T, DefaultValue, NumDimensionsRed>& operator[](const int idx) {
+        return elements[idx];
+    }
+
+//    std::tuple<int, int, T> *begin() {
+//
+//    }
+//
+//    std::tuple<int, int, T> *end() {
+//
+//    }
+
+private:
+    std::map<int, Matrix<T, DefaultValue, NumDimensionsRed>> elements;
+
+};
+
 int main() {
+    Matrix<int, -1, 2> matrix; // бесконечная матрица int заполнена значениями -1
+    assert(matrix.size() == 0); // все ячейки свободны
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+//    // выведется одна строка
+//    // 100100314
+//    for(auto c: matrix)
+//    {
+//        int x;
+//        int y;
+//        int v;
+//        std::tie(x, y, v) = c;
+//        std::cout << x << y << v << std::endl;
+//    }
     return 0;
 }
